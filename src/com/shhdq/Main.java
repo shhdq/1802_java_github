1a081438183255a45aa195a54fb4ca7f6fb7762c
KrissOzT <ozolins.kriss@gmail.com>
2022-02-18 20:46:39 +0200

    Gatavs2

--- src/com/shhdq/Main.java
+++ src/com/shhdq/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello From Kriss");
+        System.out.println("Meginats nav zaudets");
     }
 }
